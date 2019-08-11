5efb149ca23745172eca40617a3a902c0dc58c6f
juan lotito <lotybaink@hotmail.com>
2019-08-11 20:46:47 +0200

    terminado el archive de projectos

--- src/wp-content/themes/bnasociadostheme/archive-project.php
+++ src/wp-content/themes/bnasociadostheme/archive-project.php
@@ -28,18 +28,17 @@ if ( is_day() ) {
 } else if ( is_tag() ) {
 	$context['title'] = single_tag_title( '', false );
 } else if ( is_category() ) {
-	//$context['title'] = single_cat_title( '', false );
-	//array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
-        $context['termTitle'] = single_cat_title( '', false );
-        array_unshift( $templates, 'archive-project.twig' );
-        $args = array(
-            'posts_per_page' => -1,
-            'post_type' => 'project'
-        );
+	$context['title'] = single_cat_title( '', false );
+	array_unshift( $templates, 'archive-' . get_query_var( 'cat' ) . '.twig' );
 } else if ( is_post_type_archive() ) {
-	$context['title'] = post_type_archive_title( '', false );
+	$context['title'] = null;
 	array_unshift( $templates, 'archive-' . get_post_type() . '.twig' );
 }
+
+$args = array(
+    'posts_per_page' => -1,
+    'post_type' => 'project'
+);
 
 $context['posts'] = new Timber\PostQuery($args);
 
